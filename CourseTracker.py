6a849af2a10239480f27c060fc09a8df958ab1aa
Fatih Irgi <fatihirgi@gmail.com>
2025-12-14 00:12:52 +0100

    release: CourseTracker.py v1.1

--- CourseTracker.py
+++ CourseTracker.py
@@ -2,27 +2,57 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
+# without BeautifulSoup library
+import urequests
+import time
+import re
+
 URL = "https://www.ibbmeslekfabrikasi.com/tr/kursmerkezleri/57"  # Website link here
 CHECK_INTERVAL = 10                                              # Check interval (sn)
 COURSE_NAME = "KAHVE YAPIMI VE SUNUMU - HALKAPINAR KURS MERKEZİ" # Course name that be followed by
 
 # Course status getting method
 def get_course_status(url, course_name):                         
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
+    # response = requests.get(url)
+    # soup = BeautifulSoup(response.text, "html.parser")
 
-    # Find all <li> tags
-    for li in soup.find_all("li"):
+    # # Find all <li> tags
+    # for li in soup.find_all("li"):
        
-        # Check course name exist
-        if course_name in li.get_text():
-            # print(li.get_text())
-            a_tag = li.find("a")  # Find <a> tag
-            if a_tag:
-                #print(a_tag.text.strip())
-                return a_tag.text.strip()                         # "KAYIT DOLDU" or "KAYIT OL"
-    return None                                                   # Return when does not exist
+    #     # Check course name exist
+    #     if course_name in li.get_text():
+    #         # print(li.get_text())
+    #         a_tag = li.find("a")  # Find <a> tag
+    #         if a_tag:
+    #             #print(a_tag.text.strip())
+    #             return a_tag.text.strip()                         # "KAYIT DOLDU" or "KAYIT OL"
+    # return None                                                   # Return when does not exist
     
+    # without BeautifulSoup
+    try:
+        response = urequests.get(url)
+        html = response.text
+        response.close()
+    except:
+        return None
+
+    # Find all <li> ... </li> blocks
+    li_tags = re.findall(r'(?s)<li.*?>(.*?)</li>', html)
+
+    # Search for the li that includes the course name
+    for li in li_tags:
+        # Check course name exist
+        if course_name in li:
+            # Extract <a>...</a>
+            match = re.search(r'<a[^>]*>(.*?)</a>', li)
+
+            # If found, return the text inside <a> tag
+            if match:
+                print("a_tag:", match.group(1).strip())
+                return match.group(1).strip()
+
+    return None
+
 # Save first stauts
 previous_status = get_course_status(URL, COURSE_NAME)
 
