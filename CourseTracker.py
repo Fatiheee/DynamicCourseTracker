b50ebf4afff5c3137a95613552bb6c78ddcc7f51
Fatih Irgi <fatihirgi@gmail.com>
2025-12-13 23:29:01 +0100

    CourseTracker.py added

---
+++ CourseTracker.py
@@ -0,0 +1,40 @@
+import requests
+import time
+from bs4 import BeautifulSoup
+
+URL = "https://www.ibbmeslekfabrikasi.com/tr/kursmerkezleri/57"  # Website link here
+CHECK_INTERVAL = 10                                              # Check interval (sn)
+COURSE_NAME = "KAHVE YAPIMI VE SUNUMU - HALKAPINAR KURS MERKEZİ" # Course name that be followed by
+
+# Course status getting method
+def get_course_status(url, course_name):                         
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Find all <li> tags
+    for li in soup.find_all("li"):
+       
+        # Check course name exist
+        if course_name in li.get_text():
+            # print(li.get_text())
+            a_tag = li.find("a")  # Find <a> tag
+            if a_tag:
+                #print(a_tag.text.strip())
+                return a_tag.text.strip()                         # "KAYIT DOLDU" or "KAYIT OL"
+    return None                                                   # Return when does not exist
+    
+# Save first stauts
+previous_status = get_course_status(URL, COURSE_NAME)
+
+while True:
+    time.sleep(CHECK_INTERVAL)
+    current_status = get_course_status(URL, COURSE_NAME)          # Save current status
+
+    # print(current_status)
+    if current_status and current_status != previous_status:
+        print(f"{COURSE_NAME} kursunun kayıt durumu değişti!")
+        print(f"Önceki Durum: {previous_status}")
+        print(f"Şu Anki Durum: {current_status}")
+        previous_status = current_status
+    else:
+        print(f"{COURSE_NAME} kursunda değişiklik yok.")
